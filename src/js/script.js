19d97cd89ee21fb21964cae9368939a4f5d5ede6
Kamil Janusz <kamiljanusztc@gmail.com>
2021-01-12 21:47:32 +0100

    Add mixin

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+function toggleMenu(visible) {
+  document.querySelector('.menu').classList.toggle('show', visible);
+}
+
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu();
+});
+
+
