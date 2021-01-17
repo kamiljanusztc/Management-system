a695c041720ceee6efcee649ec86c5293152d7ee
Kamil Janusz <kamiljanusztc@gmail.com>
2021-01-17 18:38:26 +0100

    Add chart

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,97 @@
+const hamburger = document.querySelector('.hamburger');
+const sidebar = document.querySelector('.sidebar');
+// const mainSection = document.querySelector('.main__section');
+
+
 function toggleMenu(visible) {
-  document.querySelector('.sidebar').classList.toggle('show', visible);
+  sidebar.classList.toggle('show', visible);
 }
 
-document.querySelector('.hamburger').addEventListener('click', function(e) {
+hamburger.addEventListener('click', function(e) {
   e.preventDefault();
   toggleMenu();
 });
 
+// for (let link of mainSection) {
+//   link.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     const clickedElement = this;
 
+//     clickedElement.getAttribute('href').replace('#', '');
+
+//   });
+// }
+
+// Open modal (add show class to overlay)
+
+// Close modal (remove class 'show' from overlay)
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+// Add to buttons
+document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+// Close modal if overlay clicked
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+// Close modal if escape icon clicked
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+});
+
+// oOpen modal and close all others
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach(function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+// Diagram
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  // 1
+  type: 'bar',
+  data: {
+    // 2
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
+    // 3
+    datasets: [{
+      // 4
+      label: 'Signups',
+      // 5
+      backgroundColor: '#8DBEC8',
+      borderColor: '#8DBEC8',
+      // 6
+      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+    },
+    {
+      label: 'FTD',
+      backgroundColor: '#F29E4E',
+      borderColor: '#F29E4E',
+      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+    },
+    {
+      label: 'Earned',
+      backgroundColor: '#71B374',
+      borderColor: '#71B374',
+      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+      // 7
+      hidden: true,
+    }]
+  },
+});
